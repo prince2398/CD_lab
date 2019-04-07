772d3ea0eb2b87ab63f69f40ee1f7bd4c06354c9
prince2398 <princepiyush2398@gmail.com>
2019-04-07 19:17:02 +0530

    Added file

--- 4.NFAtoDFA/code.cpp
+++ 4.NFAtoDFA/code.cpp
@@ -1,9 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void pushNullState( vector< pair< vector<int>, unordered_map<char, vector<int> > > > &NFA, int n, vector<char> &symbols){
-    unordered_map<char, vector<int> > edges;
-    for(int i = 0 ; i < symbols.size(); ++i){
+void pushNullState( 
+    vector<
+        pair<
+            vector<int>, 
+            unordered_map<char, vector<int>>
+        >
+    > &NFA, 
+    int n, 
+    vector<char> &symbols
+    ){
+    unordered_map<char, vector<int>> edges;
+    for (int i = 0; i < symbols.size(); ++i){
         vector<int> d;
         d.push_back(n);
         edges[symbols[i]] = d;
@@ -13,9 +22,11 @@ void pushNullState( vector< pair< vector<int>, unordered_map<char, vector<int> >
     return;
 }
 
-bool alreadyExists(vector<vector<int> > &temp, vector<int> &state){
-    for(int i = 0; i < temp.size(); ++i){
-        if(temp[i] == state){
+bool alreadyExists(
+    vector<vector<int>> &temp, 
+    vector<int> &state){
+    for (int i = 0; i < temp.size(); ++i){
+        if (temp[i] == state){
             return true;
         }
     }
@@ -24,13 +35,13 @@ bool alreadyExists(vector<vector<int> > &temp, vector<int> &state){
 
 void removeDuplicates(vector<int> &t){
     unordered_set<int> s;
-    for(int i = 0 ; i < t.size(); ++i){
+    for (int i = 0; i < t.size(); ++i){
         s.insert(t[i]);
     }
-    unordered_set<int> :: iterator it = s.begin();
+    unordered_set<int>::iterator it = s.begin();
     t.clear();
 
-    while(it != s.end()){
+    while (it != s.end()){
         t.push_back(*it);
         ++it;
     }
@@ -38,13 +49,22 @@ void removeDuplicates(vector<int> &t){
     return;
 }
 
-vector<int> getDestinationStates(vector< pair< vector<int>, unordered_map<char, vector<int> > > > NFA, vector<int> state, char symbol){
+vector<int> getDestinationStates(
+    vector<
+        pair<
+            vector<int>, 
+            unordered_map<char, vector<int>>
+        >
+    > NFA, 
+    vector<int> state, 
+    char symbol
+    ){
     vector<int> ans;
-    for(int i = 0; i < NFA.size(); ++i){
+    for (int i = 0; i < NFA.size(); ++i){
         int temp = NFA[i].first[0];
-        if(find(state.begin(), state.end(), temp) != state.end()){
+        if (find(state.begin(), state.end(), temp) != state.end()){
             vector<int> tobeAdded = NFA[i].second[symbol];
-            for(int j = 0; j < tobeAdded.size(); ++j){
+            for (int j = 0; j < tobeAdded.size(); ++j){
                 ans.push_back(tobeAdded[j]);
             }
         }
@@ -53,8 +73,17 @@ vector<int> getDestinationStates(vector< pair< vector<int>, unordered_map<char, 
     return ans;
 }
 
-void PrintDFA( vector< pair< vector<int>, unordered_map<char, vector<int> > > > &dfa, vector<char> &symbols, vector<int> &final_states){
-    cout <<endl;
+void PrintDFA(
+    vector<
+        pair<
+            vector<int>,
+            unordered_map<char, vector<int>>
+        >
+    > &dfa,
+    vector<char> &symbols,
+    vector<int> &final_states
+    ){
+    cout << endl;
     cout << "State";
     cout << "  |  ";
     for (int i = 0; i < symbols.size(); ++i){
@@ -62,11 +91,11 @@ void PrintDFA( vector< pair< vector<int>, unordered_map<char, vector<int> > > > 
     }
     cout << endl;
     for (int i = 0; i < 10 * symbols.size(); ++i){
-        cout<<"-";
+        cout << "-";
     }
     cout << endl;
     for (int i = 0; i < dfa.size(); ++i){
-        cout << dfa[i].first[0]<< "      |  ";
+        cout << dfa[i].first[0] << "      |  ";
         for (int j = 0; j < symbols.size(); ++j){
             cout << dfa[i].second[symbols[j]][0] << "  |  ";
         }
@@ -76,16 +105,23 @@ void PrintDFA( vector< pair< vector<int>, unordered_map<char, vector<int> > > > 
     cout << endl;
     cout << "Final States : ";
     for (int i = 0; i < final_states.size(); ++i){
-        cout << final_states[i]<< "  ";
+        cout << final_states[i] << "  ";
     }
     cout << endl;
     return;
 }
 
-void processEdges(unordered_map<char, vector<int> > &edges, vector<int> &state, vector<int> &new_state){
-    unordered_map<char, vector<int> > :: iterator it = edges.begin();
-    while(it != edges.end()){
-        if(it->second == state){
+void processEdges(
+    unordered_map<
+        char, 
+        vector<int>
+    > &edges, 
+    vector<int> &state, 
+    vector<int> &new_state
+    ){
+    unordered_map<char, vector<int>>::iterator it = edges.begin();
+    while (it != edges.end()){
+        if (it->second == state){
             it->second = new_state;
         }
         ++it;
@@ -93,7 +129,15 @@ void processEdges(unordered_map<char, vector<int> > &edges, vector<int> &state, 
     return;
 }
 
-vector<int> processDFA(vector< pair< vector<int>, unordered_map<char, vector<int> > > > &dfa, vector<int> final_states){
+vector<int> processDFA(
+    vector<
+        pair<
+            vector<int>, 
+            unordered_map<char, vector<int>>
+        >
+    > &dfa, 
+    vector<int> final_states
+    ){
     int stateNumber = 0;
     vector<int> final;
     for (int i = 0; i < dfa.size(); ++i){
@@ -103,7 +147,12 @@ vector<int> processDFA(vector< pair< vector<int>, unordered_map<char, vector<int
             processEdges(dfa[j].second, state, new_state);
         }
         int ns = new_state[0];
-        if(find(final_states.begin(), final_states.end(), ns) != final_states.end()){
+        if (
+            find(
+                final_states.begin(), 
+                final_states.end(), 
+                ns
+            ) != final_states.end()){
             final.push_back(ns);
         }
     }
@@ -111,99 +160,119 @@ vector<int> processDFA(vector< pair< vector<int>, unordered_map<char, vector<int
 }
 
 int main(){
-    vector< pair< vector<int>, unordered_map<char, vector<int> > > > NFA;
+    vector<
+        pair<
+            vector<int>, 
+            unordered_map<char, vector<int>>
+        >
+    > NFA;
 
     int N;
     cout << "Enter the number of states in NFA: ";
     cin >> N;
-    
+
     cout << "Enter the number of symbols to be used in the NFA: ";
     int n_symbols;
     cin >> n_symbols;
-    
+
     vector<char> symbols(n_symbols);
     cout << "Enter the symbols: ";
-    for(int i = 0; i < n_symbols; ++i){
+    for (int i = 0; i < n_symbols; ++i){
         cin >> symbols[i];
     }
 
     cout << "\nEnter the NFA " << endl;
-    for(int i = 0 ; i < N; i++){
+    for (int i = 0; i < N; i++){
         cout << "\nState : " << i << endl;
-        unordered_map<char, vector<int> > edges;
+        unordered_map<char, vector<int>> edges;
         vector<int> source(1, i);
-        
-        for(int j = 0 ; j < symbols.size(); ++j){
-            cout << "\nEnter the number of edges for symbol '" << symbols[j] << "' (Enter 0 if no edge exists): ";
-            
+
+        for (int j = 0; j < symbols.size(); ++j){
+            cout<< "\nEnter the number of edges for symbol '" 
+                << symbols[j] 
+                << "' (Enter 0 if no edge exists): ";
+
             int n_edges;
             cin >> n_edges;
-            
+
             vector<int> destination(n_edges);
-            if(n_edges == 0){
+            if (n_edges == 0){
                 destination.push_back(N);
                 edges[symbols[j]] = destination;
                 continue;
             }
-            
-            cout << "Enter the states to which edges direct from " << i << " on symbol '" << symbols[j] <<  "' : " ;
-            for(int k = 0 ; k < n_edges; ++k){
+
+            cout<< "Enter the states to which edges direct from " 
+                << i 
+                << " on symbol '" 
+                << symbols[j] 
+                << "' : ";
+            for (int k = 0; k < n_edges; ++k){
                 cin >> destination[k];
             }
-            
+
             edges[symbols[j]] = destination;
             edges[symbols[j]] = destination;
         }
         NFA.push_back(make_pair(source, edges));
     }
 
-    cout<<endl;
+    cout << endl;
 
     pushNullState(NFA, N, symbols);
 
     int initial_state;
-    cout << "Enter the initial State: " ;
+    cout << "Enter the initial State: ";
     cin >> initial_state;
-    
+
     int n_final;
     cout << "Enter the number of final states: ";
     cin >> n_final;
-    
+
     vector<int> final_states(n_final);
     cout << "Enter the final states: ";
-    for(int i = 0 ; i < n_final; ++i){
+    for (int i = 0; i < n_final; ++i){
         cin >> final_states[i];
     }
-    
+
     vector<int> initial_states(1, initial_state);
-    queue<vector<int> > Q;
+    queue<vector<int>> Q;
     Q.push(initial_states);
-    
-    vector< pair< vector<int>, unordered_map<char, vector<int> > > > dfa;
-    vector<vector<int> > temp;
-    
-    while(!Q.empty()){
+
+    vector<
+        pair<
+            vector<int>, 
+            unordered_map<char, vector<int>>
+        >
+    > dfa;
+    vector<vector<int>> temp;
+
+    while (!Q.empty()){
         vector<int> state = Q.front();
         Q.pop();
-        
-        unordered_map<char, vector<int> > edges;
-        for(int i = 0; i < symbols.size(); ++i){
-            vector<int> destination = getDestinationStates(NFA, state, symbols[i]);
+
+        unordered_map<char, vector<int>> edges;
+        for (int i = 0; i < symbols.size(); ++i){
+            vector<int> destination = 
+            getDestinationStates(   NFA, 
+                                    state, 
+                                    symbols[i]
+            );
             edges[symbols[i]] = destination;
-            
-            if ( !alreadyExists(temp, destination) ){
+
+            if (!alreadyExists(temp, destination)){
                 Q.push(destination);
                 temp.push_back(destination);
             }
         }
         dfa.push_back(make_pair(state, edges));
     }
-    
+
     vector<int> dfaFinalStates = processDFA(dfa, final_states);
-    cout << endl ;
-    
+    cout << endl;
+
     cout << "Final DFA Transition Table" << endl;
     PrintDFA(dfa, symbols, dfaFinalStates);
-    
+
     return 0;
 }
